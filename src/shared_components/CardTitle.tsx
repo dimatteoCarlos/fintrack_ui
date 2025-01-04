916cb9394fe5de392be50e4dfcff0f77c460e64c
CarlosDR <dimatteoca@gmail.com>
2025-01-04 12:56:22 -0400

    delete genera_components

--- src/shared_components/CardTitle.tsx
+++
@@ -1,9 +0,0 @@
-
-
-//applied styles : generalStyles.css
-type ChildrenPropType = { children: React.ReactNode };
-
-//-----CardTitle---------------
-export function CardTitle({ children }: ChildrenPropType) {
-  return <div className='presentation__card--title'>{children} </div>;
-}
